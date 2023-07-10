ae1e7496561ad8b93084e3523aaa5213c89ae8e2
Thiago <aquino.t@tutanota.com>
2023-07-09 21:31:27 -0300

    final commit

--- mole.js
+++ mole.js
@@ -51,11 +51,20 @@ window.addEventListener('DOMContentLoaded', () => {
       clearTimeout(hideTimeout);
       hideMole(event.target);
 
-      // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
-      // event.target.classList.add('wgs__mole-head--hidden');
+       event.target.classList.add('wgs__mole-head--hidden');
 
-      // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED FOR THE BONUS
-      // event.target.classList.add('wgs__mole-head--whacked');
+     event.target.classList.add('wgs__mole-head--whacked');
     });
   }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  setInterval(() => {
+    const moleHeads = document.querySelectorAll('.wgs__mole-head');
+    for (let moleHead of moleHeads) {
+      moleHead.classList.toggle('wgs__mole-head--hidden');
+    }
+  }, 1000);
+
 });
